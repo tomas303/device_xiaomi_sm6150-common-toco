7ddebd1723fc4a0e5d154bcc1a42e1a9502a2cbb
AndroidHQ254 <d.murimi14@gmail.com>
2023-01-18 18:35:59 +0300

    Revert "sm6150-common: Settings -> SettingsGoogle, SystemUI -> SystemUIGoogle"
    This reverts commit c9c5ae6e77d2d568d508808cdd566c55efed0382.

--- sm6150.mk
+++ sm6150.mk
@@ -442,8 +442,8 @@ PRODUCT_PACKAGES += \
 
 # Preopt System Apps
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUIGoogle \
-    SettingsGoogle
+    SystemUI \
+    Settings
 
 # QTI
 PRODUCT_PACKAGES += \
